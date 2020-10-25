41e313da904345eb6f25351f81faf99639507305
quabey <quadrobtv@gmail.com>
2020-10-25 18:30:08 +0100

    start

---
+++ test-raft/test-raft.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class test-raft : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod test-raft has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod test-raft has been unloaded!");
+    }
+}
